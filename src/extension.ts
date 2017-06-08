cfd1d289f35340c743d5d3303b7f9d0816b35698
Christian Kasperbauer <chris.kasperbauer@gmail.com>
2017-06-08 22:50:11 +0200

    add basic functionality (setup config file, get reviews)

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,119 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as request from 'request';
+import * as _ from 'lodash';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { UpsConfig } from './models/UpsConfig';
+
 export function activate(context: vscode.ExtensionContext) {
+    // checkForOpenReviews();
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-upsource" is now active!');
+    // create upsource.json config file with defaults
+    let setup = vscode.commands.registerCommand('upsource.setup', () => {
+        let defaultSettings = {
+            url: '',
+            login: '',
+            password: ''
+        };
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+        let rootPath = vscode.workspace.rootPath,
+            filePath = rootPath + '/upsource.json';
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        fs.access(filePath, fs.constants.F_OK, err => {
+            if (!err) {
+                vscode.window.showInformationMessage('upsource.json already exists.');
+                showFileInTextEditor(filePath);
+                return;
+            }
+
+            fs.writeFile(filePath, JSON.stringify(defaultSettings), 'utf8', err => {
+                showFileInTextEditor(filePath);
+                vscode.window.showInformationMessage(
+                    'upsource.json has been created successfully.'
+                );
+            });
+        });
     });
 
-    context.subscriptions.push(disposable);
+    let getReviews = vscode.commands.registerCommand('upsource.getReviews', () => {
+        getConfig().then((config: UpsConfig) => {
+            request({
+                baseUrl: config.url + '/~rpc',
+                uri: '/getReviews',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Basic ' + new Buffer(config.login + ':' + config.password).toString('base64'),
+                },
+                json: true,
+                body: {
+                    projectId: config.projectId,
+                    limit: 99
+                }
+            }, (err, response, body) => {
+                if (err) {
+                    vscode.window.showInformationMessage(
+                        'Upsource server is not reachable or responded with an error.'
+                    );
+                    return;
+                }
+
+                let result = body.result,
+                    totalCount = result.totalCount,
+                    reviews = result.reviews;
+                    
+                console.log(reviews);
+                
+                if (!totalCount) vscode.window.showInformationMessage('No open Reviews.');
+                else {
+                    let items = reviews.map((review) => {
+                        return {
+                            label: review.reviewId.reviewId,
+                            detail: review.title,
+                            description: 'open'
+                        }
+                    });
+                    vscode.window.showQuickPick(items);
+                }
+            }); 
+        });
+    });
+
+    context.subscriptions.push(setup);
+    context.subscriptions.push(getReviews);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+function checkForOpenReviews() {
+    let thenable: Thenable<string | undefined> = vscode.window.showInformationMessage(
+        'There are open reviews for this project.'
+    );
 }
+
+function showFileInTextEditor(filePath) {
+    vscode.workspace.openTextDocument(filePath).then((textDocument: vscode.TextDocument) => {
+        vscode.window.showTextDocument(textDocument);
+    });
+}
+
+function getConfig(): Promise<UpsConfig> {
+    let rootPath = vscode.workspace.rootPath,
+        filePath = rootPath + '/upsource.json';
+
+    return new Promise<UpsConfig>((resolve, reject) => {
+        fs.access(filePath, fs.constants.R_OK, err => {
+            if (err) {
+                vscode.window.showInformationMessage('upsource.json is not readable.');
+                reject(err);
+                return;
+            }
+
+            fs.readFile(filePath, 'utf8', function(err, data) {
+                if (err) reject(err);
+                else resolve(<UpsConfig>JSON.parse(data));
+            });
+        });
+    });
+}
+
+export function deactivate() {}
---
+++ src/models/UpsConfig.ts
@@ -0,0 +1,8 @@
+export class UpsConfig {
+    constructor(
+        public url: string,
+        public login: string,
+        public password: string,
+        public projectId: string,
+    ) {}
+};
