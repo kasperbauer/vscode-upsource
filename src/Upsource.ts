37ac512fe6f2f5dfccf18ff63099740e2bf24a9e
Christian Kasperbauer <chris.kasperbauer@gmail.com>
2017-06-10 22:50:43 +0200

    add upsource service

---
+++ src/Upsource.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+import * as request from 'request';
+
+import Config from './Config';
+import { ReviewListDTO } from './models/ReviewListDTO';
+import { UpsConfig } from './models/UpsConfig';
+
+function sendAPIRequest(path: string, method: string, params: Object = {}): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+        let statusBarMessage = vscode.window.setStatusBarMessage(
+            'Contacting Upsource API...',
+            3000
+        );
+
+        Config.get().then(
+            (config: UpsConfig) => {
+                let body = { projectId: config.projectId };
+
+                request(
+                    {
+                        baseUrl: config.url + '/~rpc',
+                        uri: '/' + path,
+                        method: method.toUpperCase(),
+                        headers: {
+                            Authorization:
+                                'Basic ' +
+                                    new Buffer(config.login + ':' + config.password).toString(
+                                        'base64'
+                                    )
+                        },
+                        json: true,
+                        body: Object.assign(body, params)
+                    },
+                    (err, response, body) => {
+                        if (err || (response.statusCode != 200 && response.statusCode != 201)) {
+                            vscode.window.showInformationMessage(
+                                'Upsource server is not reachable or responded with an error. Please check your upsource.json.'
+                            );
+                            reject(err);
+                            return;
+                        }
+
+                        statusBarMessage.dispose();
+                        resolve(body.result);
+                    }
+                );
+            },
+            err => {
+                statusBarMessage.dispose();
+                reject(err);
+            }
+        );
+    });
+}
+
+function getReviewListWithState(state: string): Promise<ReviewListDTO> {
+    let params = {
+        limit: 99,
+        query: state ? 'state: ' + state : ''
+    };
+
+    return sendAPIRequest('getReviews', 'POST', params);
+}
+
+export default {
+    sendAPIRequest,
+    getReviewListWithState
+};
--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import * as request from 'request';
 import * as opn from 'opn';
 
 import Config from './Config';
+import Upsource from './Upsource';
 import { ReviewListDTO } from './models/ReviewListDTO';
 import { UpsConfig } from './models/UpsConfig';
 
@@ -33,7 +34,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function checkForOpenReviews() {
-    getReviewListWithState('open').then(
+    Upsource.getReviewListWithState('open').then(
         res => {
             if (res.totalCount) {
                 vscode.window.showInformationMessage(
@@ -48,7 +49,7 @@ function checkForOpenReviews() {
 }
 
 function showReviewQuickPicks(state?: string) {
-    getReviewListWithState(state).then(res => {
+    Upsource.getReviewListWithState(state).then(res => {
         let totalCount = res.totalCount,
             reviews = res.reviews;
 
@@ -81,63 +82,6 @@ function showReviewQuickPicks(state?: string) {
                 });
             });
         }
-    });
-}
-
-function getReviewListWithState(state: string): Promise<ReviewListDTO> {
-    let params = {
-        limit: 99,
-        query: state ? 'state: ' + state : ''
-    };
-
-    return sendAPIRequest('getReviews', 'POST', params);
-}
-
-function sendAPIRequest(path: string, method: string, params: Object = {}): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-        let statusBarMessage = vscode.window.setStatusBarMessage(
-            'Contacting Upsource API...',
-            3000
-        );
-
-        Config.get().then(
-            (config: UpsConfig) => {
-                let body = { projectId: config.projectId };
-
-                request(
-                    {
-                        baseUrl: config.url + '/~rpc',
-                        uri: '/' + path,
-                        method: method.toUpperCase(),
-                        headers: {
-                            Authorization:
-                                'Basic ' +
-                                    new Buffer(config.login + ':' + config.password).toString(
-                                        'base64'
-                                    )
-                        },
-                        json: true,
-                        body: Object.assign(body, params)
-                    },
-                    (err, response, body) => {
-                        if (err || (response.statusCode != 200 && response.statusCode != 201)) {
-                            vscode.window.showInformationMessage(
-                                'Upsource server is not reachable or responded with an error. Please check your upsource.json.'
-                            );
-                            reject(err);
-                            return;
-                        }
-
-                        statusBarMessage.dispose();
-                        resolve(body.result);
-                    }
-                );
-            },
-            err => {
-                statusBarMessage.dispose();
-                reject(err);
-            }
-        );
     });
 }
 
