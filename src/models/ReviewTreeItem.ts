8fb521743c0aa0ddb99b75a00b940d1e0114e8cc
Christian Kasperbauer <christian.kasperbauer@webfactor.de>
2017-07-13 16:22:49 +0200

    add review tree item model

---
+++ src/models/ReviewTreeItem.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+import { ReviewDescriptorDTO } from './ReviewDescriptorDTO';
+
+export class ReviewTreeItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly review?: ReviewDescriptorDTO
+    ) {
+        super(label, collapsibleState);
+    }
+}
